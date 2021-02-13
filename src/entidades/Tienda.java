aa30d3306af6e609104332f540695cb9fcaf57f6
maria23051982 <77923900+maria23051982@users.noreply.github.com>
2021-02-13 10:20:24 -0300

    Update Tienda.java
    Hola chicos, acá una versión (ahora lo documento en trello). Perdón con los acentos, pero tengo el teclado para atrás!!!

--- src/entidades/Tienda.java
+++ src/entidades/Tienda.java
@@ -1,12 +1,18 @@
-package entidades;
+package TiendaFotografa;
 
-public class Tienda {
-	private String Dueño;
-	private String Direccion;
-	private String Telefono;
+public abstract class Fotografia {
+
+	public static void main(String[] args) {
 		
-	
-	
-	
-	
+		String Tienda = "Tienda Fotografica grupo 1";
+		String Direccion = "Avenida De Los Trabajadores 4500"+ " Mar del Pata";
+		String Telefono = "2234698800";
+		
+		System.out.println(Tienda );
+		System.out.println(Direccion);
+		System.out.println(Telefono);
+                System.out.println("**************************************************");
+        
+	}
+
 }
